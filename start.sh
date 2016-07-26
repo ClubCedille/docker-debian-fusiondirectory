92d210c3c3ca406713463cbf13d548dbb941b0fc
Michaël Faille <michael@faille.io>
2016-07-26 01:14:20 -0400

    FIX : move block code to wait
    Move it before ldap setup from fusion directory

--- start.sh
+++ start.sh
@@ -15,6 +15,13 @@ export LDAP_DOMAIN_DC="dc=$(echo ${SLDAP_DOMAIN} | sed  's/\./,dc=/g')"
 
 envsubst < /fusiondirectory.conf > /etc/fusiondirectory/fusiondirectory.conf
 
+
+# Wait tcp connection to ldap server
+while [  ! cat < /dev/tcp/${LDAP_SERVER}/386 ]; do
+    sleep 1
+done
+
+
 if [ ! -e "/etc/fusiondirectory/fusionready" ]; then
     yes Yes | fusiondirectory-setup --check-config
     fusiondirectory-setup --yes --check-ldap << EOF
@@ -24,10 +31,5 @@ $FUSIONDIRECTORY_PASSWORD
 EOF
     touch /etc/fusiondirectory/fusionready
 fi
-
-# Wait tcp connection to ldap server
-while [  ! cat < /dev/tcp/${LDAP_SERVER}/386 ]; do
-    sleep 1
-done
 
 gosu www-data sh -c ". /etc/apache2/envvars && /usr/sbin/apache2 -D FOREGROUND"
